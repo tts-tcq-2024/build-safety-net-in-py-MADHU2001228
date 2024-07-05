f887eb8d1e97fbb8a2777d29ad7a55889c594882
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-07-05 06:35:23 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -10,24 +10,39 @@ def get_soundex_code(c):
     }
     return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
+def initialize_soundex(name):
+    # Initialize the soundex code with the first letter
+    first_letter = name[0].upper()
+    return first_letter, get_soundex_code(first_letter)
+
+def should_add_code(char, code, prev_code):
+    return code != '0' and code != prev_code
+
+def add_code_to_soundex(soundex, code):
+    return soundex + code
+
+def is_length_four(soundex):
+    return len(soundex) == 4
+
+def process_characters(name, soundex, prev_code):
+    # Generate codes and filter out '0' and codes that are the same as prev_code
+    codes = filter(lambda code: code != '0' and code != prev_code, (get_soundex_code(char) for char in name[1:]))
+    
+    # Join the valid codes and take the first 3 characters
+    soundex += ''.join(codes)[:3]
+    
+    # Pad with zeros if necessary to ensure length is 4
+    return soundex.ljust(4, '0')
+
+
+def pad_soundex(soundex):
+    # Pad with zeros if necessary to ensure length is 4
+    return soundex.ljust(4, '0')
 
 def generate_soundex(name):
     if not name:
         return ""
 
-    # Start with the first letter (capitalized)
-    soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
-
-    for char in name[1:]:
-        code = get_soundex_code(char)
-        if code != '0' and code != prev_code:
-            soundex += code
-            prev_code = code
-        if len(soundex) == 4:
-            break
-
-    # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
-
-    return soundex
+    soundex, prev_code = initialize_soundex(name)
+    soundex = process_characters(name, soundex, prev_code)
+    return pad_soundex(soundex)
