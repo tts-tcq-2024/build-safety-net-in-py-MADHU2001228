4edb1937d65e08dddf03a8301adcddcc8028000a
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-07-05 07:13:48 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -11,17 +11,30 @@ def get_soundex_code(c):
     return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
 def initialize_soundex(name):
+    # Initialize the soundex code with the first letter
     first_letter = name[0].upper()
     return first_letter, get_soundex_code(first_letter)
 
-def process_characters(name, prev_code):
-    # Generate codes for characters in the name starting from the second character
-    codes = (get_soundex_code(char) for char in name[1:])
-    # Filter out '0' codes and consecutive duplicates
-    filtered_codes = (code for code in codes if code != '0' and code != prev_code)
-    return ''.join(filtered_codes)[:3]  # Take the first 3 valid codes
+def should_add_code(char, code, prev_code):
+    return code != '0' and code != prev_code
+
+def add_code_to_soundex(soundex, code):
+    return soundex + code
+
+def is_length_four(soundex):
+    return len(soundex) == 4
+
+def process_characters(name, soundex, prev_code):
+    # Generate codes and filter out '0' and codes that are the same as prev_code
+    codes = filter(lambda code: code != '0' and code != prev_code, (get_soundex_code(char) for char in name[1:]))
+    
+    # Join the valid codes and take the first 3 characters
+    soundex += ''.join(codes)[:3]
+    
+    return soundex
 
 def pad_soundex(soundex):
+    # Pad with zeros if necessary to ensure length is 4
     return soundex.ljust(4, '0')
 
 def generate_soundex(name):
@@ -29,5 +42,5 @@ def generate_soundex(name):
         return ""
 
     soundex, prev_code = initialize_soundex(name)
-    soundex += process_characters(name, prev_code)
+    soundex = process_characters(name, soundex, prev_code)
     return pad_soundex(soundex)
