0eda39d3b399f569908bd1bc6d2586351bc5115a
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-07-05 06:38:47 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -9,6 +9,21 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
 
+    def test_two_characters(self):
+        self.assertEqual(generate_soundex("AB"), "A100")
+
+    def test_three_characters(self):
+        self.assertEqual(generate_soundex("ABC"), "A120")
+
+    def test_name_with_similar_sounding_letters(self):
+        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+
+    def test_name_with_varying_length(self):
+        self.assertEqual(generate_soundex("Tymczak"), "T522")
+
+    def test_generate_soundex_edge_cases(self):
+        self.assertEqual(generate_soundex("B2B"), "B100")
+
     
 if __name__ == '__main__':
     unittest.main()
